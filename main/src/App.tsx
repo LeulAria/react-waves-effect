3aebc536f946ae9d30130bdd42c70773048846b1
Leul <leulsegedtariku@gmail.com>
2021-07-10 13:36:54 +0300

    🎉 INT COMMIT: init

---
+++ main/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import { Uxium } from './uxium';
+
+function App() {
+  return (
+    <div className="App">
+      <Uxium>Uxium here</Uxium>
+    </div>
+  )
+}
+
+export default App
---
+++ main/src/uxium/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Uxium = ({ children }: { children: string }) => {
+  return (
+    <div>
+      Form Library {children}
+    </div>
+  )
+}
